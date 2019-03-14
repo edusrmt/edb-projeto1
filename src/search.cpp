a63eff8f74dbf6dfa8e48bd9ca3c4594195ba5e3
Carlos Eduardo Alves Sarmento <edusrmt@A21214037397L.imd.local>
2019-03-14 20:27:34 -0300

    Busca binária melhorada

--- src/search.cpp
+++ src/search.cpp
@@ -3,70 +3,95 @@
 #include <algorithm>
 #include <math.h>
 
+void print_step (long int vector[], int first, int last) {
+    // Prints out the original data container.
+    std::cout << "Array: [ ";
+    for (int i = first; i <= last; i++)
+        std::cout << vector[i] << " ";    
+
+    std::cout << "]\n";
+}
+
 int lsearch(long int vector[], int last, long int value) {
-    int first = 0;
-    while(first != last) {
-        if(vector[first] == value) {
-            return first;
-        } else {
-            first++;
-        }
+    for (int i = 0; i <= last; i++) {
+        if (vector[i] == value)
+            return i;
     }
+
     return -1;
 }
 
-int bsearch(long int vector[], int last, long int value) {
+int bsearch_i(long int vector[], int last, long int value) {
     int first = 0;
-    while((last - first) > 1) {
-        int sz = last - first;
-        int middle = sz/2;
-        long int x = vector[first+middle];
+    int length = last + 1;
 
-        if(x == value) {
-            return first+middle;
-        } else if(x < value) {
-            first += middle;
-        } else if(x > value) {
-            last -= middle;
+    while (first <= last) {
+        int middle = length / 2 + first;
+
+        if (vector[middle] == value)
+            return middle;
+        else if (vector[middle] < value)
+            first = middle + 1;
+        else {
+            last = middle - 1;
         }
 
-        if((last - first) == 1) {
-            if(value == vector[first]) {
-                return first;
-            } else if(value == vector[last]) {
-                return last;
-            }
-        }
-
+        length = last - first + 1;
     }
+
     return -1;
 }
 
-int bsearch(long int vector[], int first, int last, long int value) {
-    if((last - first) > 1) {
-        int sz = last - first;
-        int middle = sz/2;
-        long int x = vector[first+middle];
+int bsearch_r(long int vector[], int last, long int value, int first = 0) {
+    int length = last - first + 1;
 
-        if(x == value) {
-            return first+middle;
-        } else if(x < value) {
-            first += middle;
-        } else if(x > value) {
-            last -= middle;
-        }
-        return bsearch(vector, first, last, value);
+    if (first <= last) {
+        int middle = length / 2 + first;
 
-        if((last - first) == 1) {
-            if(value == vector[first]) {
-                return first;
-            } else if(value == vector[last]) {
-                return last;
-            }
+        if (vector[middle] == value)
+            return middle;
+        else {
+            if (vector[middle] < value)
+                first = middle + 1;
+            else
+                last = middle - 1;
+
+            return bsearch_r(vector, last, value, first);
         }
-    } else {
-        return -1;
     }
+
+    return -1;
+}
+
+int tsearch_i(long int vector[], int last, long int value) {
+    int first = 0;
+    while (last - first > 2) {
+        int sz = last - first;
+        int indexA = first + sz;
+        int indexB = indexA + sz;
+
+        if (vector[indexA] == value) {
+            return indexA;
+        } else if (vector[indexA] < value) {
+            first = indexA;
+
+            if (vector[indexB] == value) {
+                return indexB;
+            } else if (vector[indexB] < value) {
+                first = indexB;
+            } else if (vector[indexB] > value) {
+                last = indexB;
+            }
+        } else if (vector[indexA] > value) {
+            last = indexA;
+        }
+    }
+
+    if (last - first == 2) {
+
+    }
+    
+    return -1;
 }
 
 int jsearch(long int vector[], int last, long int value) {
@@ -90,7 +115,7 @@ int jsearch(long int vector[], int last, long int value) {
 
 int main() {
     long int A[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Data container.
-    long int targets[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 , -4, 20 }; // Target values for testing.
+    long int targets[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -4, 20}; // Target values for testing.
 
     // Prints out the original data container.
     std::cout << "Array: [ ";
@@ -104,7 +129,7 @@ int main() {
     {
         // Look for target in the entire range.
         //auto result = const_cast<value_type *>(lsearch(std::begin(A), std::end(A), e));
-        int result = jsearch(A, 9, e);
+        int result = bsearch_r(A, 9, e);
 
         // Process the result
         if(result != -1)
