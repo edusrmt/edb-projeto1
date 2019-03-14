ea6d4f9e281f800f14cfbcf994295b7d9576f6f9
Victor Vieira <victorvieira89@gmail.com>
2019-03-14 14:30:19 -0300

    Busca linear e Busca binária adicionadas

--- src/search.cpp
+++ src/search.cpp
@@ -2,6 +2,45 @@
 #include <iterator>
 #include <algorithm>
 
+int lsearch(int vector[], int first, int last, int value)
+{
+    while(first != last) {
+        if(vector[first] == value) {
+            return first;
+        } else {
+            first++;
+        }
+    }
+    return -1;
+}
+
+int bsearch(int vector[], int first, int last, int value)
+{
+    while((last - first) > 1) {
+        int sz = last - first;
+        int middle = sz/2;
+        int x = vector[first+middle];
+
+        if(x == value) {
+            return first+middle;
+        } else if(x < value) {
+            first += middle;
+        } else if(x > value) {
+            last -= middle;
+        }
+
+        if((last - first) == 1) {
+            if(value == vector[first]) {
+                return first;
+            } else if(value == vector[last]) {
+                return last;
+            }
+        }
+
+    }
+    return -1;
+}
+
 int main()
 {
 
