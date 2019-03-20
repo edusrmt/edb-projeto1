203e38cc50629676a45008712ff5a90f9f0b7b4e
Victor Vieira <victorvieira89@gmail.com>
2019-03-19 21:28:17 -0300

    Versão final

--- docs/html/search/all_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['value_5ftype',['value_type',['../namespaceft.html#a076c091630cdeb4076ec793da6ab56c0',1,'ft']]]
-];
--- docs/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['1_2e_20introduction',['1. Introduction',['../md__r_e_a_d_m_e.html',1,'']]]
-];
